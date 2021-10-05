576d1019758ddc768b6b54da60b645ec4b4b6581
floriane <floriane@flodebian>
2021-10-05 16:03:21 +0200

    add loading on page load

--- src/containers/CV/Header/index.js
+++ src/containers/CV/Header/index.js
@@ -2,12 +2,12 @@ import React, {useContext} from 'react';
 import {cvContext} from "../../../context/cvContext";
 
 import Link from '../../../styles/Links';
-import {CvHeaderStyle, CvHeadline, CvTitle, UserData, Address, Contact} from "./headerStyles";
+import {StyledCvHeader, CvHeadline, CvTitle, UserData, Address, Contact} from "./headerStyles";
 import CategoryTitle from '../../../styles/CategoryTitle';
 
 
 function CvHeader(props) {
-    const {userData} = useContext(cvContext);
+    const {userData, isLoading} = useContext(cvContext);
 
     
     const userAddress = userData.address;
@@ -24,37 +24,41 @@ function CvHeader(props) {
     
 
     return(
-        <CvHeaderStyle>
-            <CvTitle >
-                {userData.name}
-                <CvHeadline>{userData.tagline}</CvHeadline>
-            </CvTitle>
-            
-            <UserData>
-                <CategoryTitle>CONTACT</CategoryTitle >
-                <div>
-                    <Address>
-                        {addressArray[0]},<br />
-                        {addressArray[1]},<br />
-                        {addressArray[2]}
-                    </Address>
-
-                    <Contact>
-                        <p><Link href={`tel:${userPhoneLink}`}>{userData["phone_number"]}</Link></p>
-
-                        {/* How to mix strings and JS script here */}
-                        <p><Link href={`mailto:${userData.email }`} >{userData.email}</Link></p>
-                        <p>
-                            <Link href={`https://${userData.website}`}>
-                                https://{userData.website}</Link>
-                        </p>
-                    </Contact>
-                </div>
+        <StyledCvHeader>
+            {!isLoading ? 
+            <>
+                <CvTitle >
+                    {userData.name}
+                    <CvHeadline>{userData.tagline}</CvHeadline>
+                </CvTitle>
                 
+                <UserData>
+                    <CategoryTitle>CONTACT</CategoryTitle >
+                    <div>
+                        <Address>
+                            {addressArray[0]},<br />
+                            {addressArray[1]},<br />
+                            {addressArray[2]}
+                        </Address>
 
-                
-            </UserData>
-        </CvHeaderStyle>
+                        <Contact>
+                            <p><Link href={`tel:${userPhoneLink}`}>{userData["phone_number"]}</Link></p>
+
+                            {/* How to mix strings and JS script here */}
+                            <p><Link href={`mailto:${userData.email }`} >{userData.email}</Link></p>
+                            <p>
+                                <Link href={`https://${userData.website}`}>
+                                    https://{userData.website}</Link>
+                            </p>
+                        </Contact>
+                    </div>
+                    
+
+                    
+                </UserData>
+            </> : <CvTitle>Loading...</CvTitle>
+        }
+        </StyledCvHeader>
     );
 }
 
